800e02e149f5a464338ace9de2a069c98599b273
Noah Lanfrit <noahlanfrit@outlook.com>
2021-06-15 17:34:35 +0200

    update API

--- test.js
+++ test.js
@@ -3,35 +3,13 @@ const Search = require("./main");
 // const search = new Search("socks5h://127.0.0.1:9050");
 const search = new Search();
 
-let page = 1;
-let interval = 5;
-
-search.getCookieAsync((resultCookie) => {
-  if (resultCookie.success) {
-    console.log("START");
-    // search.setCookie(resultCookie.cookie);
-    search.setPage(page);
-    startSearch();
-  } else {
-    console.log("FALSE");
-  }
-});
-
-function startSearch() {
-  setTimeout(
-    () =>
-      search.search((result) => {
-        if (result.cookie) {
-          search.setCookie(result.cookie);
-        }
-        if (result.code == 200) {
-          page++;
-          search.setPage(page);
-          console.log(`NEW PAGE : ${page}`);
-        }
-        console.log(result.cookie, result.code);
-        startSearch();
-      }),
-    interval * 1000
-  );
+function start() {
+  search.getDataPage((result) => {
+    console.log(result);
+    setTimeout(() => {
+      start();
+    }, 5000);
+  });
 }
+
+start();
